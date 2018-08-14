da41adee1b9b4be72c06f7dc4c13e8175c08e7b3
volosgoto <volosgoto@yahoo.com>
2018-08-14 15:42:33 +0300

    add getText function

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // FETCH API
+
+document.getElementById('button1').addEventListener('click', getText);
+
+function getText(e) {
+    // console.log('Get Text'); 
+    fetch('test.txt')  // return's promise
+        .then(function (response) { 
+            // console.log(response);
+            return response.text();  // 
+        })
+        .then(function (data) { // grab data from promise
+            console.log(data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })  
+}
