c83d348ad885bfc6e0f809c06124dd011386daca
volosgoto <volosgoto@yahoo.com>
2018-08-14 15:55:44 +0300

    add getJson function

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // FETCH API
 
 document.getElementById('button1').addEventListener('click', getText);
+document.getElementById('button2').addEventListener('click', getJson);
 
+
+// Get local file data
 function getText(e) {
     // console.log('Get Text'); 
     fetch('test.txt')  // return's promise
@@ -10,7 +13,30 @@ function getText(e) {
             return response.text();  // 
         })
         .then(function (data) { // grab data from promise
-            console.log(data);
+            //  console.log(data);
+            document.getElementById('output').innerHTML = data;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })  
+}
+
+
+// Get local JSON data
+function getJson(e) {
+    fetch('users.json')  // return's promise
+        .then(function (response) { 
+            // console.log(response);
+            return response.json();  // 
+        })
+        .then(function (data) { // grab data from promise
+            // console.log(data);
+            let output = '';
+            data.forEach(user => {
+                output += `<li>ID:${user.id} Name: ${user.name} Company: ${user.company} Phone: ${user.phone}</li>`
+            });
+
+            document.getElementById('output').innerHTML = output;
         })
         .catch(function (error) {
             console.log(error);
