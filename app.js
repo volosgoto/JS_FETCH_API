6c066af6cad458807ea55d44c7609c0906be0322
volosgoto <volosgoto@yahoo.com>
2018-08-15 09:45:43 +0300

    refactor to arrow functions

--- app.js
+++ app.js
@@ -6,31 +6,59 @@ document.getElementById('button3').addEventListener('click', getExternal);
 
 
 // Get local file data
+// function getText() {
+//     // console.log('Get Text'); 
+//     fetch('test.txt')  // return's promise
+//         .then(function (response) { 
+//             // console.log(response);
+//             return response.text();  // 
+//         })
+//         .then(function (data) { // grab data from promise
+//             //  console.log(data);
+//             document.getElementById('output').innerHTML = data;
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         })  
+// }
+
+
+// Get local file data
 function getText() {
-    // console.log('Get Text'); 
-    fetch('test.txt')  // return's promise
-        .then(function (response) { 
-            // console.log(response);
-            return response.text();  // 
-        })
-        .then(function (data) { // grab data from promise
-            //  console.log(data);
-            document.getElementById('output').innerHTML = data;
-        })
-        .catch(function (error) {
-            console.log(error);
-        })  
+    fetch('test.txt')
+        .then(response => response.text())
+        .then(data => document.getElementById('output').innerHTML = data)
+        .catch(error => console.log(error))
 }
+
+
+// Get local JSON data
+// function getJson() {
+//     fetch('users.json')  // return's promise
+//         .then(function (response) {  
+//             // console.log(response);
+//             return response.json();  // 
+//         })
+//         .then(function (data) { // grab data from promise
+//             // console.log(data);
+//             let output = '';
+//             data.forEach(user => {
+//                 output += `<li>ID:${user.id} Name: ${user.name} Company: ${user.company} Phone: ${user.phone}</li>`
+//             });
+
+//             document.getElementById('output').innerHTML = output;
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         })  
+// }
 
 
 // Get local JSON data
 function getJson() {
     fetch('users.json')  // return's promise
-        .then(function (response) {  
-            // console.log(response);
-            return response.json();  // 
-        })
-        .then(function (data) { // grab data from promise
+        .then(response => response.json())
+        .then(data => { // grab data from promise
             // console.log(data);
             let output = '';
             data.forEach(user => {
@@ -38,31 +66,46 @@ function getJson() {
             });
 
             document.getElementById('output').innerHTML = output;
+
         })
-        .catch(function (error) {
-            console.log(error);
-        })  
+        .catch(error => console.log(error));
 }
 
+
+// Get External
+// function getExternal() {
+//     fetch('https://api.github.com/users')  // return's promise
+//         .then(function (response) {  
+//             // console.log(response);
+//             return response.json();  // 
+//         })
+//         .then(function (data) { // grab data from promise
+//             // console.log(data);
+//             let output = '';
+//             data.forEach(user => {
+//                 output += `<li>ID:${user.id} Login: ${user.login}</li>`
+//             });
+
+//             document.getElementById('output').innerHTML = output;
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         })  
+// }
 
 
 // Get External
 function getExternal() {
     fetch('https://api.github.com/users')  // return's promise
-        .then(function (response) {  
-            // console.log(response);
-            return response.json();  // 
-        })
-        .then(function (data) { // grab data from promise
-            // console.log(data);
+        .then(response => response.json()) 
+        .then(data => {
             let output = '';
             data.forEach(user => {
                 output += `<li>ID:${user.id} Login: ${user.login}</li>`
             });
 
             document.getElementById('output').innerHTML = output;
+
         })
-        .catch(function (error) {
-            console.log(error);
-        })  
+        .catch(error => console.log(error))
 }
